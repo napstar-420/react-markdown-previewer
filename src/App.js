8733bb403ebfaa3b5f27c861798ce79c7cfb8ecc
zohaib khan <zohaibkhan1162002@gmail>
2022-07-03 11:09:43 -0700

    Commiting everything

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import {useState} from 'react';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import './Scss/main.css'
+
 function App() {
-  return <h1>React App</h1>;
+  const [markdownText, setMarkdownText] = useState('');
+
+  return (
+    <div className="App">
+      <div className="editor-container">
+        <h1>Editor</h1>
+        <textarea name="editor" id="editor" rows='10' cols='50' style={{resize: 'none'}} value={markdownText} onChange={(e)=>setMarkdownText(e.target.value)}/>
+      </div>
+      <div className="preview-container">
+        <h1>Preview</h1>
+        <div id="preview">
+          <ReactMarkdown remarkPlugins={[gfm]}>{markdownText}</ReactMarkdown>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
