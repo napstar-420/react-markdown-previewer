6c8c69c633650844ffdb0a9ac838ea7706cd1e90
zohaib khan <zohaibkhan1162002@gmail>
2022-07-04 00:25:58 -0700

    Added initial Markdown text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,43 @@ import './Scss/main.css';
 
 function App() {
   const [markdownText, setMarkdownText] = useState('');
-  
+
+  const setInitialMarkup =() =>{
+    setMarkdownText(
+      `# Welcome to my React Markdown
+## This is a sub-heading...
+### And here's some other cool stuff:`
+.concat('\r\n Heres some code, `<div></div>`, between 2 backticks.\r\n')
+.concat(`\r\n You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~. 
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes! \r\n
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+        
+        1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`)
+    )
+  }
+
+  useEffect(()=>{
+    setInitialMarkup();
+  },[])
+
   return (
     <div className="App">
       <div className="editor-container">
